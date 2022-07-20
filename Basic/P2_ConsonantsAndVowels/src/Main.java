7066b0ee9b07840bf4b211f13fe4fd1c9560a8d5
La Quoc Thang <quocthang_0507@yahoo.com.vn>
2022-07-20 11:50:22 +0700

    Update code

--- Basic/P2_ConsonantsAndVowels/src/Main.java
+++ Basic/P2_ConsonantsAndVowels/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    public static void Check(char input) {
+        boolean isVowel = false;
+        switch (input) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                isVowel = true;
+        }
+        if (isVowel)
+            System.out.println(input + " is vowel");
+        else {
+            if ((input >= 'a' && input <= 'z') || (input >= 'A' && input <= 'Z'))
+                System.out.println(input + " is consonant");
+            else
+                System.out.println(input + " is not in alphabet");
+        }
     }
 }
