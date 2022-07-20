01abfc794452299ea2c89a468b3aae5e1b042c21
La Quoc Thang <quocthang_0507@yahoo.com.vn>
2022-07-20 16:45:41 +0700

    Add P3

---
+++ Basic/P3_LCM/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, lcm;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        b = scanner.nextInt();
+        scanner.close();
+        lcm = LCM(a, b);
+        System.out.println("Least common multiple of " + a + " and " + b + " is " + lcm);
+    }
+
+    public static int LCM(int a, int b) {
+        int lcm = a > b ? a : b;
+        while (true) {
+            if (lcm % a == 0 && lcm % b == 0)
+                return lcm;
+            ++lcm;
+        }
+    }
+}
