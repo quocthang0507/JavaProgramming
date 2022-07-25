0ce8be93cc197c6e6bc21ae3b7a3b015efc30114
La Quoc Thang <quocthang_0507@yahoo.com.vn>
2022-07-25 08:34:33 +0700

    Update code

--- Basic/P4_GetIPAddress/src/Main.java
+++ Basic/P4_GetIPAddress/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) throws UnknownHostException {
 
         InetAddress myIp = InetAddress.getLocalHost();
+        String hostname = myIp.getHostName();
         System.out.println("My IP address is " + myIp.getHostAddress());
+        System.out.println("My hostname is " + hostname);
     }
 }
